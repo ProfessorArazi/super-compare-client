d650ab5af7299b029fb1bbf2b8e2222a53f64c42
ProfessorArazi <amitarazi123456.gmail.com>
2024-10-12 15:59:22 +0300

    changed a tag to div

--- src/components/Cart/Result/Result.jsx
+++ src/components/Cart/Result/Result.jsx
@@ -1,30 +1,30 @@
 import classes from "./Result.module.css";
 
 const Result = ({ prices, onClose }) => {
+    const navigateHandler = (link) => {
+        window.open(link, "_blank");
+    };
+
     return (
         <>
             <div className={classes.grid}>
                 {prices
                     .sort((a, b) => a[1].price - b[1].price)
                     .map((price) => (
-                        <a
+                        <div
+                            onClick={() => navigateHandler(price[1].url)}
                             key={price[1].url}
-                            href={price[1].url}
-                            target="_blank"
-                            rel="noopener noreferrer"
                             className={classes.card}
                         >
                             <h3>{price[0]}</h3>
                             <p>מחיר : {price[1].price.toFixed(2)}</p>
-                            <a
-                                href={price[1].url}
-                                target="_blank"
-                                rel="noopener noreferrer"
+                            <div
+                                onClick={() => navigateHandler(price[1].url)}
                                 className={classes.marketButton}
                             >
                                 הזמנה
-                            </a>
-                        </a>
+                            </div>
+                        </div>
                     ))}
             </div>
             <div className={classes.backButtonContainer}>
