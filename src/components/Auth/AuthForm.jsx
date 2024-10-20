bb04303c9eb145aa2130e6b67a164f49003ba0f1
ProfessorArazi <amitarazi123456@gmail.com>
2024-10-20 20:50:28 +0300

    loading on auth

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -3,6 +3,7 @@ import classes from "./AuthForm.module.css";
 import Modal from "../UI/Modal/Modal";
 import { signin, signup } from "../../services/auth-api";
 import FavoritesContext from "../../store/Favorites/favorites-context";
+import LoadingSpinner from "../UI/LoadingSpinner/LoadingSpinner";
 
 const AuthForm = ({ onClose, isVerified, setIsVerified }) => {
     const [email, setEmail] = useState("");
@@ -10,6 +11,7 @@ const AuthForm = ({ onClose, isVerified, setIsVerified }) => {
     const [type, setType] = useState("login");
     const [errorMessage, setErrorMessage] = useState(null);
     const [successMessage, setSuccessMessage] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const ctx = useContext(FavoritesContext);
 
@@ -19,6 +21,7 @@ const AuthForm = ({ onClose, isVerified, setIsVerified }) => {
             const user = { email, password };
             setErrorMessage(null);
 
+            setIsLoading(true);
             const response = await signin(user);
 
             if (response?.data?.token) {
@@ -39,6 +42,8 @@ const AuthForm = ({ onClose, isVerified, setIsVerified }) => {
                 error.response?.data?.message ||
                     "משהו השתבש, נסה שוב מאוחר יותר..."
             );
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -48,6 +53,7 @@ const AuthForm = ({ onClose, isVerified, setIsVerified }) => {
             const user = { email, password };
             setErrorMessage(null);
 
+            setIsLoading(true);
             const response = await signup(user);
 
             if (response?.data?.id) {
@@ -62,6 +68,8 @@ const AuthForm = ({ onClose, isVerified, setIsVerified }) => {
                 error.response?.data?.message ||
                     "משהו השתבש, נסה שוב מאוחר יותר..."
             );
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -74,64 +82,72 @@ const AuthForm = ({ onClose, isVerified, setIsVerified }) => {
 
     return (
         <Modal onClose={onClose}>
-            <div className={classes.formContainer}>
-                <form
-                    onSubmit={type === "login" ? signinHanlder : signupHanlder}
-                >
-                    {isVerified && (
-                        <div className={classes.successMessage}>
-                            המשתמש אומת בהצלחה
-                        </div>
-                    )}
-                    <h3 className={classes.title}>
-                        {type === "login" ? "כניסה" : "הרשמה"}
-                    </h3>
-                    {errorMessage && (
-                        <div className={classes.errorMessage}>
-                            {errorMessage}
-                        </div>
-                    )}
-                    {successMessage && (
-                        <div className={classes.successMessage}>
-                            {successMessage}
-                        </div>
-                    )}
-                    <div className={classes.formGroup}>
-                        <input
-                            placeholder="אימייל"
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className={classes.formGroup}>
-                        <input
-                            placeholder="סיסמה"
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <button type="submit" className={classes.button}>
-                        {type === "login" ? "כניסה" : "הרשמה"}
-                    </button>
-                </form>
-
-                <div className={classes.toggleText}>
-                    {type === "login" ? "עדיין לא רשום?" : "כבר רשום?"}{" "}
-                    <button
-                        type="button"
-                        onClick={toggleType}
-                        className={classes.toggleButton}
+            {
+                <div className={classes.formContainer}>
+                    <form
+                        onSubmit={
+                            type === "login" ? signinHanlder : signupHanlder
+                        }
                     >
-                        {type === "login" ? "הרשם כאן" : "התחבר כאן"}
-                    </button>
+                        {isVerified && (
+                            <div className={classes.successMessage}>
+                                המשתמש אומת בהצלחה
+                            </div>
+                        )}
+                        {isLoading ? (
+                            <LoadingSpinner />
+                        ) : (
+                            <h3 className={classes.title}>
+                                {type === "login" ? "כניסה" : "הרשמה"}
+                            </h3>
+                        )}
+                        {errorMessage && (
+                            <div className={classes.errorMessage}>
+                                {errorMessage}
+                            </div>
+                        )}
+                        {successMessage && (
+                            <div className={classes.successMessage}>
+                                {successMessage}
+                            </div>
+                        )}
+                        <div className={classes.formGroup}>
+                            <input
+                                placeholder="אימייל"
+                                type="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className={classes.formGroup}>
+                            <input
+                                placeholder="סיסמה"
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button type="submit" className={classes.button}>
+                            {type === "login" ? "כניסה" : "הרשמה"}
+                        </button>
+                    </form>
+
+                    <div className={classes.toggleText}>
+                        {type === "login" ? "עדיין לא רשום?" : "כבר רשום?"}{" "}
+                        <button
+                            type="button"
+                            onClick={toggleType}
+                            className={classes.toggleButton}
+                        >
+                            {type === "login" ? "הרשם כאן" : "התחבר כאן"}
+                        </button>
+                    </div>
                 </div>
-            </div>
+            }
         </Modal>
     );
 };
